eb4a85af36992cd240a654ed5966eb9d070ca558
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-09-22 21:39:49 +0600

    auth add

--- src/route/api/index.js
+++ src/route/api/index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const router = express.Router()
 
-// console.log("api route working")
-router.get("/", (req,res)=>{
-    return res.send("api route working .....")
-})
+// router.get("/", (req,res)=>{
+//     return res.send("api route working .....")
+// })
 
+// http://localhost:3000/api/v1/auth
+const auth = require('./auth')
+router.use('/auth', auth)
 
 module.exports = router;
--- src/route/index.js
+++ src/route/index.js
@@ -2,9 +2,11 @@ const express = require("express")
 const router = express.Router()
 
 console.log("route working")
-// connect with api folder
-const api = require("./api")   
 
+// connect with api folder
+const api = require("./api")
+
+// http://localhost:3000/api/v1
 router.use(process.env.BASE_URL , api)
 
 module.exports = router;
