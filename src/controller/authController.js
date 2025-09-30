67dc959d6f04c8323e8baebd19f41e105d85274e
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-09-30 15:52:36 +0600

    OTP send

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,10 +1,11 @@
 const userModel = require("../model/signup.model");
+const generateOTP = require("../utils/otp");
 const sendEmail = require("../utils/send-email");
 
 const signupController = async (req, res, next) => {
   let { name, email, password, phone, image, role } = req.body;
 
-
+  let otp = generateOTP()
 
   let user = new userModel({
     name,
@@ -16,7 +17,7 @@ const signupController = async (req, res, next) => {
   });
 
   await user.save().then(() => {
-    sendEmail(email)
+    sendEmail(email, otp)
     return res
       .status(201)
       .json({ success: true, message: "user created successfull", data: user })
