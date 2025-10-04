cb46dd251497eda8d067f809a8a374e93d461a4b
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-10-04 22:11:44 +0600

    invalid pass or email

--- src/route/api/auth/index.js
+++ src/route/api/auth/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { signupController, verifyOtpController } = require("../../../controller/authController");
+const { signupController, verifyOtpController, loginController } = require("../../../controller/authController");
 const router = express.Router()
 
 // http://localhost:3000/api/v1/auth/signup
 router.post("/signup",signupController)
+// http://localhost:3000/api/v1/auth/verify-otp
 router.post("/verify-otp", verifyOtpController)
+// http://localhost:3000/api/v1/auth/login
+router.post("/login", loginController) 
 
-module.exports = router; 
+module.exports = router;
