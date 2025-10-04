276b358bffde1e4e7dd75f81106c7996d1d3a906
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-10-04 13:06:36 +0600

    create an account

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -5,55 +5,73 @@ const sendEmail = require("../utils/send-email");
 const signupController = async (req, res, next) => {
   let { name, email, password, phone, image, role } = req.body;
 
-  let otp = generateOTP();
+  let userfind = await userModel.findOne({ email });
 
-  let user = new userModel({
-    name,
-    email,
-    password,
-    phone,
-    image,
-    role,
-    otp,
-  });
-
-  await user.save().then(() => {
-    sendEmail(email, otp);
-
-    // setTimeout(async () => {
-    //   let otpremove = await userModel.findOneAndUpdate({email}, {otp:null}, {new:true})
-    //   await otpremove.save().then(()=>{
-    //     console.log("otp remove")
-    //   })
-    // }, 60000);
-
-    let info = {
-      name: user.name,
-      email: user.email,
-    }
+  if (userfind) {
     return res
-      .status(201)
-      .json({ success: true, message: "user created successfull", data: user })
-      .catch((err) => {
-        next(err);
-      });
-  });
+      .status(500)
+      .json({ success: false, message: "email already exist" });
+  } else {
+    let otp = generateOTP();
+
+    let user = new userModel({
+      name,
+      email,
+      password,
+      phone,
+      image,
+      role,
+      otp,
+    });
+
+    await user.save().then(() => {
+      sendEmail(email, otp);
+
+      // setTimeout(async () => {
+      //   let otpremove = await userModel.findOneAndUpdate({email}, {otp:null}, {new:true})
+      //   await otpremove.save().then(()=>{
+      //     console.log("otp remove")
+      //   })
+      // }, 60000);
+
+      let info = {
+        name: user.name,
+        email: user.email,
+      };
+      return res
+        .status(201)
+        .json({
+          success: true,
+          message: "user created successfull",
+          data: info,
+        })
+        .catch((err) => {
+          next(err);
+        });
+    });
+  }
 };
 
-const verifyOtpController = async (req, res, next)=>{
-  let {email, otp} = req.body
+const verifyOtpController = async (req, res, next) => {
+  let { email, otp } = req.body;
 
-  let user =await userModel.findOne({email})
-  if(!user){
-    return res.status(404).json({success:false, message:"user not found"})
-  }else{
-    if(user.otp === otp){
-      let verify = await userModel.findOneAndUpdate({email}, {verify:true, otp:null}, {new:true}.select("-password"))
-      return res.status(200).json({success:true, message:"otp verified", data:verify})
-    }else{
-    return res.status(404).json({success:false, message:"otp not match"})
+  let user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(404).json({ success: false, message: "user not found" });
+  } else {
+    if (user.otp === otp) {
+      let verify = await userModel.findOneAndUpdate(
+        { email },
+        { verify: true, otp: null },
+        { new: true }.select("-password")
+      );
+      return res
+        .status(200)
+        .json({ success: true, message: "otp verified", data: verify });
+    } else {
+      return res.status(404).json({ success: false, message: "otp not match" });
+    }
   }
-  }
-}
+};
 
-module.exports = { signupController, verifyOtpController }; 
+module.exports = { signupController, verifyOtpController };
