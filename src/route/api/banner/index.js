a92f9a9f6154cc878069a937a4e6c272d3ecebe4
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-10-11 14:48:48 +0600

    delete api done

--- src/route/api/banner/index.js
+++ src/route/api/banner/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const { addBannerController, deleteBannerController } = require("../../../controller/bannerController");
-const router = express.Router()
 const upload = require("../../../utils/addbannerMiddleware");
 const { TokenCheckMiddelware, adminCheck } = require("../../../utils/authMiddleware");
+const router = express.Router()
 
 // http://localhost:3000/api/v1/banner/addbanner
 router.post("/addbanner",TokenCheckMiddelware,adminCheck, upload.single("banner") , addBannerController)
