3b50961a56f4e67afd812eed8ad3d8f5877ded2b
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-10-11 13:37:07 +0600

    create a middleware

--- src/controller/bannerController.js
+++ src/controller/bannerController.js
@@ -1,21 +1,28 @@
-const bannerModel = require("../model/banner.model")
+const bannerModel = require("../model/banner.model");
 
 let addBannerController = async (req, res) => {
-    // console.log(req.body)
-    // return res.send(req.files)
-    let {link} = req.body
-    let {filename} = req.file
-    try {
-        let banner = await new bannerModel({
-            image : `${process.env.SERVER_URL}/${filename}`,
-            link
-        })
-        await banner.save()
 
-        return res.status(201).json({success:true, message: "banner created successfull", data: banner})
-    } catch (error) {
-        return res.status(500).json({success:false, message: error.message|| error})
-    }
-}
+  let { link } = req.body;
+  let { filename } = req.file;
+  try {
+    let banner = await new bannerModel({
+      image: `${process.env.SERVER_URL}/${filename}`,
+      link,
+    });
+    await banner.save();
 
-module.exports = {addBannerController}
+    return res
+      .status(201)
+      .json({
+        success: true,
+        message: "banner created successfull",
+        data: banner,
+      });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: error.message || error });
+  }
+};
+
+module.exports = { addBannerController };
--- src/route/api/banner/index.js
+++ src/route/api/banner/index.js
@@ -1,40 +1,9 @@
 const express = require("express");
 const { addBannerController } = require("../../../controller/bannerController");
-const multer  = require('multer')
 const router = express.Router()
-//bannerMiddleware start
-const path = require('path')
+const upload = require("../../../utils/addbannerMiddleware");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads')
-  },
-  filename: function (req, file, cb) {
-    console.log(file)
-    const randomtext = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    let fileextension = file.originalname.split(".")
-    let extension = fileextension[fileextension.length-1]
-    cb(null, file.fieldname + '-' + randomtext + "." + extension)
-  }
-})
-
-function checkFileType(file, cb) {
-    const filetypes = /jpeg|jpg|png|gif/;
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = filetypes.test(file.mimetype);
-
-    if(mimetype && extname) {
-        return cb(null, true);
-    } else {
-        cb('Error: images only! (jpeg, jpg, png, gif)')
-    }
-}
-
-const upload = multer({ storage: storage, limits:{fileSize:2000000}, fileFilter:function(req, file, cb){
-    checkFileType(file, cb)
-} })
-//bannerMiddleware end
 // http://localhost:3000/api/v1/banner/addbanner
-router.post("/addbanner", upload.array("banner") , addBannerController)
+router.post("/addbanner", upload.single("banner") , addBannerController)
 
 module.exports = router;
